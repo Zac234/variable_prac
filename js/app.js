a84453a4e38ec6f0a6867c0496355076d3f3ccc1
Zach <zacbenincasa@gamil.com>
2022-07-05 20:57:37 -0600

    added more

--- js/app.js
+++ js/app.js
@@ -16,6 +16,8 @@ if(user_age>18){
 let user_points=1-100
 if (user_points>=75){
     console.log("you have enough points to proceed")
+}else if(user_points = 50){
+    console.log("you are so close!!")
 }else {
     console.log("if you can't do it, keep trying, eventually you will make progress.")
 }
